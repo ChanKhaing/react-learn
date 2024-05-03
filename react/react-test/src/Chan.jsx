58fb3eb7b8c2ce3c5abd07402eadcf7f8f71af24
Sitnyein <chankhinem@gmail.com>
2024-05-04 02:58:27 +0630

    second commit

---
+++ react/react-test/src/Chan.jsx
@@ -0,0 +1,6 @@
+const Chan = () => {
+  return (
+     <span className = "mt-5"> Chan khine </span>
+  ) }
+
+export default Chan
--- react/react-test/src/main.jsx
+++ react/react-test/src/main.jsx
@@ -6,7 +6,7 @@
 
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./index.css"
+import "./style.css";
 
 
 // import 
