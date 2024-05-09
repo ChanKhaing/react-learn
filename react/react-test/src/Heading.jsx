8119d6812a7f43a6b6f4c338b43acc8f45ff7db3
Sitnyein <chankhinem@gmail.com>
2024-05-09 15:19:04 +0630

    nov9

--- react/react-test/src/Heading.jsx
+++ react/react-test/src/Heading.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function Heading() {
   return (
-    <h1 className="text-3xl font-bold mb-5 font-heading">List Builder App</h1>
+    <h1 className="text-3xl font-bold mb-5 font-heading">Todo App</h1>
   );
 }
 
