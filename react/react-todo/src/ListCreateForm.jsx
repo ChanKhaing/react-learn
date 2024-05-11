c2a926d2961a1a1da0f7b6b8e942901d5708def5
Sitnyein <chankhinem@gmail.com>
2024-05-11 20:04:57 +0630

    todo

---
+++ react/react-todo/src/ListCreateForm.jsx
@@ -0,0 +1,34 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const ListCreateForm = (props) => {
+  return (
+
+    <div className="flex mb-5">
+      {props.createlist("chan")}
+      <input
+        type="text"
+        id="textInput"
+        className="border-2 border-zinc-700 focus-visible:outline-none flex-grow px-3"
+      />
+      <button id="addBtn" className="bg-zinc-700 text-zinc-50 p-4">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="w-4 h-4"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 4.5v15m7.5-7.5h-15"
+          />
+        </svg>
+      </button>
+    </div>
+  );
+}
+
+export default ListCreateForm
