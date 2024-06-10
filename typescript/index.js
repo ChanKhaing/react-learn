8a8add314be8495235983fa8d4c92485cc46a0ec
Sitnyein <chankhinem@gmail.com>
2024-06-10 12:23:58 +0630

    mon10

---
+++ typescript/index.js
@@ -0,0 +1,15 @@
+console.log("hello world");
+var num = 1;
+var num1 = 2;
+var chan = /** @class */ (function () {
+    function chan() {
+    }
+    return chan;
+}());
+console.log("chankhine");
+var person = {
+    name: "chan",
+    age: 23,
+    male: true,
+};
+console.log(person.name);
