abb2eaea6c97f25ffc96b4d7126e6024218bbcc1
Sitnyein <chankhinem@gmail.com>
2024-10-20 07:30:50 +0630

    voucherlara

--- 14-voucher-app-lara-api/src/pages/DashboardPage.jsx
+++ 14-voucher-app-lara-api/src/pages/DashboardPage.jsx
@@ -5,48 +5,34 @@ import {
   HiCircleStack,
   HiComputerDesktop,
   HiDocumentDuplicate,
-  HiUserCircle,
 } from "react-icons/hi2";
-import Logout from "../components/Logout";
 
 const DashboardPage = () => {
   return (
     <section>
       <Container>
-        <div className=" grid  grid-cols-2 md:grid-cols-3 gap-5 mb-10">
+        <div className=" grid  grid-cols-1 md:grid-cols-3 grid-rows-3 gap-5">
           <div className=" col-span-1 row-span-1">
             <ModuleBtn
               name={"Product Module"}
               icon={<HiCircleStack className=" size-14" />}
-              url={"/dashboard/product"}
+              url={"/product"}
             />
           </div>
           <div className=" col-span-1 row-span-1">
             <ModuleBtn
               name={"Sale Module"}
               icon={<HiComputerDesktop className=" size-14" />}
-              url={"/dashboard/sale"}
+              url={"/sale"}
             />
           </div>
           <div className=" col-span-1 row-span-1">
             <ModuleBtn
               name={"Voucher Module"}
               icon={<HiDocumentDuplicate className=" size-14" />}
-              url={"/dashboard/voucher"}
+              url={"/voucher"}
             />
           </div>
-          {/* <div className=" col-span-1 row-span-1">
-            <ModuleBtn
-              name={"User Profile"}
-              icon={<HiUserCircle className=" size-14" />}
-              url={"/dashboard/user-profile"}
-            />
-          </div> */}
-        </div>
-
-        <div className="flex gap-3 items-center justify-end">
-          <p>If you finish your job, just </p>
-          <Logout />
         </div>
       </Container>
     </section>
